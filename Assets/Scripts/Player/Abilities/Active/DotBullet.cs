348ed8a3028a3db02a2394247385201981e25bb5
Shlmp <daniel.kil@uniat.edu.mx>
2024-04-09 19:41:10 -0600

    Added & Fixed DOT Asset

--- Assets/Scripts/Player/Abilities/Active/DotBullet.cs
+++ Assets/Scripts/Player/Abilities/Active/DotBullet.cs
@@ -14,23 +14,3 @@ public class DotBullet : ActiveAbility
         player.GetComponentInChildren<PointAndShoot>().FireBullet(bullet, bulletSpeed);
     }
 }
-
-//Temporary placed BigShot.cs script here in order to test DOT, CreateAssetMenu does not work for me, idk why
-/**
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-
-[CreateAssetMenu]
-public class BigShot : ActiveAbility
-{
-    public GameObject bullet;
-    public float bulletSpeed;
-    public override void ActivateAbility(GameObject player)
-    {
-        Debug.Log("Shoot go big");
-        player.GetComponentInChildren<PointAndShoot>().FireBullet(bullet, bulletSpeed);
-    }
-}
-**/
