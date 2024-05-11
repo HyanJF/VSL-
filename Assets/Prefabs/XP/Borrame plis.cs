263f2dc502963b485c5a599ae30a3cde13c9574d
Jared Osorno <jaredlares5@gmail.com>
2024-05-11 14:45:04 -0600

    seteo xp

--- Assets/Prefabs/XP/Borrame plis.cs
+++ Assets/Prefabs/XP/Borrame plis.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Borrame3 : MonoBehaviour
+public class Borrameplis : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
