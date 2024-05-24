bc808c8dfef41e2f9ba9da6031b24b6d9e5e93c7
HF <hyan.fierros@uniat.edu.mx>
2024-05-24 10:35:11 -0600

    UI_MainMenuFinishFinal

--- Assets/Scripts/Hyan_Scritps/UI_MainMenu/MainMenu_UI1.cs
+++ Assets/Scripts/Hyan_Scritps/UI_MainMenu/MainMenu_UI1.cs
@@ -9,6 +9,8 @@ public class MainMenu_UI1 : MonoBehaviour
 
     public GameObject creditsMenu;
 
+    public GameObject extraMenu;
+
     public GameObject blackPanel;
 
     public void Options()
@@ -31,10 +33,20 @@ public class MainMenu_UI1 : MonoBehaviour
         StartCoroutine(Black(creditsMenu, mainMenu));
     }
 
+    public void Extra()
+    {
+        StartCoroutine(Black(mainMenu, extraMenu));
+    }
+
+    public void ReturnMenuExtra()
+    {
+        StartCoroutine(Black(extraMenu, mainMenu));
+    }
+
     IEnumerator Black(GameObject Ob1, GameObject Ob2)
     {
         blackPanel.SetActive(true);
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1);
         blackPanel.SetActive(false);
         Ob1.SetActive(false);
         Ob2.SetActive(true);
