60c33a81cb4b3c22bd3a60fe624a3f16816ca558
hf <hyan.fierros@uniat.edu.mx>
2024-05-23 23:49:01 -0600

    UI_MaIinMenuBeta

--- Assets/Scripts/Hyan_Scritps/UI_MainMenu/MainMenu_UI1.cs
+++ Assets/Scripts/Hyan_Scritps/UI_MainMenu/MainMenu_UI1.cs
@@ -4,11 +4,45 @@ using UnityEngine;
 
 public class MainMenu_UI1 : MonoBehaviour
 {
-    public Animator animator;
-    public GameObject OptionsMenu;
-    public void StartGame()
+    public GameObject optionsMenu;
+    public GameObject mainMenu;
+
+    public GameObject creditsMenu;
+
+    public GameObject blackPanel;
+
+    public void Options()
     {
-       animator.SetBool("Close", true); 
+        StartCoroutine(Black(mainMenu, optionsMenu));
     }
 
+    public void ReturnMenuOptions()
+    {
+        StartCoroutine(Black(optionsMenu, mainMenu));
+    }
+
+    public void Credits()
+    {
+        StartCoroutine(Black(mainMenu, creditsMenu));
+    }
+
+    public void ReturnMenuCredits()
+    {
+        StartCoroutine(Black(creditsMenu, mainMenu));
+    }
+
+    IEnumerator Black(GameObject Ob1, GameObject Ob2)
+    {
+        blackPanel.SetActive(true);
+        yield return new WaitForSeconds(2);
+        blackPanel.SetActive(false);
+        Ob1.SetActive(false);
+        Ob2.SetActive(true);
+    }
+
+    public void ExitGame() 
+    {
+        Debug.Log("Game is exiting");
+        Application.Quit();
+    }
 }
